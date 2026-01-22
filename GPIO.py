e2a1c79e31de4a9d478338980af5007272acb9f8
Atharv Bhalerao <imathrocker@gmail.com>
2026-01-22 14:25:08 -0800

    Function generator works, Now the lock in amplifier.

--- GPIO.py
+++ GPIO.py
@@ -55,7 +55,7 @@ class dmm():
 
 
 class fg():
-    def __init__(self, device_file = '/dev/ttyUSB0', channel = 21):
+    def __init__(self, device_file = '/dev/ttyUSB0', channel = 19):
         # begin serial 
         self.ser = serial.Serial(device_file,timeout=3)
         # Clear any pending buffers
@@ -142,9 +142,36 @@ class fg():
         self.SendCommand("*IDN?")
         print(self.ReadSingle())
 
+class lockin():
+    def __init__(self, device_file = '/dev/ttyUSB0', channel = 23):
+        # begin serial 
+        self.ser = serial.Serial(device_file,timeout=3)
+        # Clear any pending buffers
+        self.ser.flushOutput()
+        self.ser.flushInput()
+        # Pushed the prologix adapter to controller mode
+        self.ser.write("++mode 1\r".encode())
+        # Disables auto-read mode, can enable later on when needed
+        self.ser.write("++auto 0\r".encode())
+        # Asserts end of message line at end of each line
+        self.ser.write("++eoi 1\r".encode())
+        self.channel = channel
+        addr_string = "++addr " + str(self.channel) + "\r"
+        self.ser.write(addr_string.encode())
+        self.ser.flushOutput()
+        self.ser.flushInput()
+
+    def SendCommand(self, command):
+        cmdstr = command + "\r"
+        self.ser.write(cmdstr.encode())
+    
+    def ReadSingle(self):
+        self.ser.write("++read eoi\r".encode())
+        value = self.ser.readline()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        return value
 
 if __name__ == "__main__":
-    fg = fg()
-    fg.id()
-    fg.config("SINE",1200,10.0)
-    fg.ON()
+    SR510 = lockin()
+    SR510.SendCommand("G 22")
